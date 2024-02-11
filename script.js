b803739ee7fc9f977a73ef9a43ad9379ea8a579e
annisssimo <anyalar04@gmail.com>
2024-02-11 20:02:13 +0300

    feat: apply consistent styling to HTML markup and enhance button interaction

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function Book(title, author, numberOfPages, isRead) {
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
 const theHobbit2 = new Book('The Hobbit2', 'J.R.R. Tolkien2', 2952, 'read');
 
-
 function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
     return myLibrary;
