a2fad1ceba499f525fac189d98de9bfd9ec3cb5f
annisssimo <anyalar04@gmail.com>
2024-02-12 14:33:45 +0300

    feat: implement displayEachBook function

--- script.js
+++ script.js
@@ -10,14 +10,51 @@ function Book(title, author, numberOfPages, isRead) {
   }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
-const theHobbit2 = new Book('The Hobbit2', 'J.R.R. Tolkien2', 2952, 'read');
-
 function addBookToLibrary(newBook) {
-    myLibrary.push(newBook);
-    return myLibrary;
+  myLibrary.push(newBook);
+  return myLibrary;
 }
 
-function displayEachBook() {
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+const harryPotter = new Book('Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling', 309, 'read');
+const toKillAMockingbird = new Book('To Kill a Mockingbird', 'Harper Lee', 281, 'read');
+const theGreatGatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 180, 'not read yet');
+const romeoAndJuliet = new Book('Romeo and Juliet', 'William Shakespeare', 187, 'read');
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(harryPotter);
+addBookToLibrary(toKillAMockingbird);
+addBookToLibrary(theGreatGatsby);
+addBookToLibrary(romeoAndJuliet);
+
+console.log(myLibrary);
+
+
+//loops through the array and displays each book on the page
+function displayEachBook(lib) {
+  const booksGridContainer = document.querySelector('.books-grid-container')
+  
+  lib.forEach(book => {
+    const bookCard = document.createElement('div');
+    bookCard.className = 'book-card';
     
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+    const numberOfPages = document.createElement('p');
+    const isRead = document.createElement('p');
+
+    bookTitle.innerHTML = `<span>Book title:</span> ${book.title}`;
+    bookAuthor.innerHTML = `<span>Author:</span> ${book.author}`;
+    numberOfPages.innerHTML = `<span>Pages:</span> ${book.numberOfPages}`;
+    isRead.innerHTML = book.isRead;
+
+
+    booksGridContainer.appendChild(bookCard);
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(numberOfPages);
+    bookCard.appendChild(isRead);
+  });
 }
+
+displayEachBook(myLibrary);
